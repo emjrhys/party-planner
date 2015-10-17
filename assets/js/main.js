b1ade6eaf220a16891367fc3b95d0551211b820b
Noah Laferty <noah.lafferty@gmail.com>
2015-10-17 18:52:23 -0400

    created new branch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 var apiKey = '85sxht8kfrf85keus8du5z8u';
-// var url = 'http://api.walmartlabs.com/v1/feeds/items?apiKey=85sxht8kfrf85keus8du5z8u&categoryId=3944';
-var url = 'http://api.walmartlabs.com/v1/taxonomy?apiKey=' + apiKey
+var url = 'http://api.walmartlabs.com/v1/feeds/items?apiKey=' + apiKey + '&categoryId=3891_532459_1019843';
+// var url = 'http://api.walmartlabs.com/v1/taxonomy?apiKey=' + apiKey
 
 $.getJSON(url, function(data) {
 	console.log(data);
