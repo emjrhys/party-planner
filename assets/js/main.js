317b0c07fd8a3ab0029b0b7cda663d9feb5617ce
Noah Laferty <noah.lafferty@gmail.com>
2015-10-17 19:03:14 -0400

    api queries working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 var apiKey = '85sxht8kfrf85keus8du5z8u';
-var url = 'http://api.walmartlabs.com/v1/feeds/items?apiKey=' + apiKey + '&categoryId=3891_532459_1019843';
+var url = 'http://api.walmartlabs.com/v1/search?apiKey=85sxht8kfrf85keus8du5z8u&query=ipod&categoryId=3944';
 // var url = 'http://api.walmartlabs.com/v1/taxonomy?apiKey=' + apiKey
 
 $.getJSON(url, function(data) {
