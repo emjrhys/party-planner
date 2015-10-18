e4ef9a272ede4ac0e8da2587c9c67799a6c05654
Apple <apple@pal-nat186-159-221.itap.purdue.edu>
2015-10-18 01:54:43 -0400

    grant's submission

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,10 @@ angular.module('lego', [])
 	$scope.items = []; 
 	$scope.cart = [];
 	$scope.removed = [];
+	$scope.totalCost = 0;
+	$scope.wTotalCost = 0;
+	var searchIds = [];
+	var searchParam = 0;
 
 	$.getJSON("data/whatever.json", function(data){
 		console.log(data);
@@ -28,15 +32,34 @@ angular.module('lego', [])
 	$scope.addToCart = function(idx) {
 		$scope.cart.push($scope.items[idx]);
 		$scope.items.splice(idx, 1);
+		updateCost();
 	}
 
 	$scope.removeFromCart = function(idx) {
 		$scope.removed.push($scope.cart[idx]);
 		$scope.cart.splice(idx, 1);
+		updateCost();
 	}
 
 	$scope.removeFromList = function(idx) {
 		$scope.removed.push($scope.items[idx]);
 		$scope.items.splice(idx,1);
+	}
+
+	function updateCost() {
+		console.log("updating cost");
+		$scope.totalCost = $scope.cart.reduce(function(pv, cv) {
+			if(cv.msrp != null) return pv + cv.msrp;
+			else return pv + cv.salePrice}, 0);
+		$scope.wTotalCost = $scope.cart.reduce(function(pv, cv) {
+			if(cv.salePrice != null) return pv + cv.salePrice;
+			else return pv + 0}, 0);
+		console.log($scope.totalCost);
+	}
+
+	$scope.updateSearchParam = function() {
+		$scope.cart.forEach(function(e) {searchIds.push(e.itemId)});
+		searchParam = searchIds.join(",");
+		console.log(searchParam);
 	}
 });
