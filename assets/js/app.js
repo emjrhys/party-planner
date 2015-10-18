28c707ba70ba590262d11d1231673cd4c57a8e10
Apple <apple@pal-nat186-159-221.itap.purdue.edu>
2015-10-17 20:46:48 -0400

    grants first git

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,19 +6,35 @@ angular.module('lego', [])
 
 	$scope.items = []; 
 	$scope.cart = [];
+	$scope.removed = [];
 
-	$.ajax({
-	  url: url,
-	  dataType: "jsonp",
-	  success: function (data) {
-	  	console.log(data);
-	    $scope.items = data.items;
-	    $scope.$apply();
-	  }
+	$.getJSON("data/whatever.json", function(data){
+		console.log(data);
+		$scope.items = data.items;
+		$scope.$apply();
 	});
+	// $.ajax({
+	//   url: url,
+	//   dataType: "jsonp",
+	//   success: function (data) {
+	//   	console.log(data);
+	//     $scope.items = data.items;
+	//     $scope.$apply();
+	//   }
+	// });
 
 	$scope.addToCart = function(idx) {
 		$scope.cart.push($scope.items[idx]);
 		$scope.items.splice(idx, 1);
+	}
+
+	$scope.removeFromCart = function(idx) {
+		$scope.removed.push($scope.cart[idx]);
+		$scope.cart.splice(idx, 1);
+	}
+
+	$scope.removeFromList = function(idx) {
+		$scope.removed.push($scope.items(idx));
+		$scope.items.splice(idx,1);
 	}
 });
